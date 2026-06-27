ac2cf81c60c1ffa0c6d0938899417b7e8d448557
Aravind C Raju <acraju@merpsystems.com>
2026-06-27 12:41:32 +0530

    Drop Motivation buttons; default repo -> Arvcrj/shorts-automation; SW v9

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Minimal service worker: caches the app shell so it installs + opens offline.
-const CACHE = "shorts-trigger-v8";
+const CACHE = "shorts-trigger-v9";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest",
                "./icon-192.png", "./icon-512.png"];
 
